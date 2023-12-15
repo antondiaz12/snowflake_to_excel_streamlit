70ed0589091c4d29c2785e7e72acc692ba497c6b
antondiaz12 <152857934+antondiaz12@users.noreply.github.com>
2023-12-15 13:09:18 +0100

    testing connection

--- extract_load_app.py
+++ extract_load_app.py
@@ -3,3 +3,11 @@ import pandas
 import requests #New section to display fruityvice API response
 import snowflake.connector
 from urllib.error import URLError
+
+streamlit.title("My Mom's New Healthy Diner")
+
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
