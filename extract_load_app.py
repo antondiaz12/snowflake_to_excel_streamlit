64e44fa216d4f9429a84fffff410f1f991732564
Maru <152593620+marucarrillog@users.noreply.github.com>
2023-12-15 14:45:38 +0100

    Update extract_load_app.py

--- extract_load_app.py
+++ extract_load_app.py
@@ -6,4 +6,11 @@ from urllib.error import URLError
 
 streamlit.title("Exportación de SF a Excel")
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
 
