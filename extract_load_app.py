eefc88e34665766abd3f304b86ddae00ea39bcd9
Maru <152593620+marucarrillog@users.noreply.github.com>
2023-12-15 14:52:30 +0100

    Update extract_load_app.py

--- extract_load_app.py
+++ extract_load_app.py
@@ -9,8 +9,19 @@ streamlit.title("Exportación de SF a Excel")
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 streamlit.text("Fruit list")
-streamlit.text(my_data_row)
+streamlit.text(my_data_rows)
+
+st.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = st.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    st.error("Please select a fruit to get information")
+  else:
+    back_from_function = get_fruityvice_data(fruit_choice)
+    st.dataframe(back_from_function)
+except URLError as e:
+  st.error()
 
 
