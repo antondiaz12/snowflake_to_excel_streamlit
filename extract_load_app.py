1dd23c7e28c632eca81f83f8dd6d5334d549095c
Maru <152593620+marucarrillog@users.noreply.github.com>
2023-12-15 15:10:17 +0100

    Update extract_load_app.py

--- extract_load_app.py
+++ extract_load_app.py
@@ -11,8 +11,49 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.dataframe(my_data_rows)
+streamlit.dataframe.columns = ['Fruits']
+
+## NEW SECTION 
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
+st.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = st.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    st.error("Please select a fruit to get information")
+  else:
+    back_from_function = get_fruityvice_data(fruit_choice)
+    st.dataframe(back_from_function)
+except URLError as e:
+  st.error()
+
+st.header("View Our Fruit List - Add You Favorites!")
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+        my_cur.execute("select * from fruit_load_list")
+        return my_cur.fetchall()
+
+if st.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  my_cnx.close()
+  st.dataframe(my_data_rows)
+
+
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values ('"+new_fruit+"')")
+    return "Thanks for adding " + new_fruit
+
 add_my_fruit = st.text_input('What fruit would you like to add?')
-streamlit.button('Add a Fruit to the List')
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-back_from_function = insert_row_snowflake(add_my_fruit)
-streamlit.text(back_from_function)
+if st.button('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  st.text(back_from_function)
+  
+
+## STOP!!
+st.stop()
