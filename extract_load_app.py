847c7e89a5361a2793381d4f37038b3fedf6973c
antondiaz12 <152857934+antondiaz12@users.noreply.github.com>
2023-12-15 13:11:15 +0100

    adding title

--- extract_load_app.py
+++ extract_load_app.py
@@ -4,10 +4,6 @@ import requests #New section to display fruityvice API response
 import snowflake.connector
 from urllib.error import URLError
 
-streamlit.title("My Mom's New Healthy Diner")
+streamlit.title("Exportación de SF a Excel")
 
-streamlit.header('Breakfast Favorites')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
+
