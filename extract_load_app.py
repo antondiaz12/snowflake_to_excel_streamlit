f65475b7d25dfed29eb677be5846460c6197c96d
Maru <152593620+marucarrillog@users.noreply.github.com>
2023-12-15 14:54:20 +0100

    Update extract_load_app.py

--- extract_load_app.py
+++ extract_load_app.py
@@ -11,17 +11,7 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.text("Fruit list")
-streamlit.text(my_data_rows)
+streamlit.dataframe(my_data_rows)
 
-st.header("Fruityvice Fruit Advice!")
-try:
-  fruit_choice = st.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-    st.error("Please select a fruit to get information")
-  else:
-    back_from_function = get_fruityvice_data(fruit_choice)
-    st.dataframe(back_from_function)
-except URLError as e:
-  st.error()
 
 
