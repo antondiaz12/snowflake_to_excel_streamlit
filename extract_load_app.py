df46b989207f07e34849613f8b40af6adb15e404
Maru <152593620+marucarrillog@users.noreply.github.com>
2024-01-02 16:48:19 +0100

    Update extract_load_app.py

--- extract_load_app.py
+++ extract_load_app.py
@@ -7,26 +7,26 @@ from urllib.error import URLError
 streamlit.title("From Streamlit to Snowflake")
 streamlit.header("Check out our fruit list!")
 
-'''def get_fruityvice_data(this_fruit_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  return fruityvice_normalized'''
+#def get_fruityvice_data(this_fruit_choice):
+#  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+#  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#  return fruityvice_normalized
 
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
         my_cur.execute("select * from fruit_load_list")
         return my_cur.fetchall()
     
-'''streamlit.header("Fruityvice Fruit Advice!")
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-    streamlit.error("Please select a fruit to get information")
-  else:
-    back_from_function = get_fruityvice_data(fruit_choice)
-    streamlit.dataframe(back_from_function)
-except URLError as e:
-  streamlit.error()'''
+#streamlit.header("Fruityvice Fruit Advice!")
+#try:
+#  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+#  if not fruit_choice:
+#    streamlit.error("Please select a fruit to get information")
+#  else:
+#    back_from_function = get_fruityvice_data(fruit_choice)
+#    streamlit.dataframe(back_from_function)
+#except URLError as e:
+#  streamlit.error()
 
 if streamlit.button('Get Fruit List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
@@ -114,7 +114,7 @@ else:
     streamlit.text("Please check the fields")
 
 ## TABLE 
-streamlit.header("More detailed information about the list of fruits!!")
+streamlit.header("See detailed information on each fruit")
 if streamlit.button('Get info'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   fruit_info = fruityvice_selected()
