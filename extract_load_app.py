10992b158bc7fa1cba134e146f8e0a520ec0d76c
Maru <152593620+marucarrillog@users.noreply.github.com>
2024-01-02 10:54:01 +0100

    Update extract_load_app.py

--- extract_load_app.py
+++ extract_load_app.py
@@ -35,7 +35,7 @@ if streamlit.button('Get Fruit List'):
   my_cnx.close()
   table = pandas.DataFrame(my_data_rows)
   table.columns = ["Fruits"]
-  table.set_index("Fruits", inplace=True)
+  table = table.reset_index()
   info = streamlit.dataframe(table)
 
 
